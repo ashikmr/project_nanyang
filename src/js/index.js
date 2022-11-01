eae8a25a0b8074792ef20dce73b4684fac173b62
ashikmr <ashishkmr735@gmail.com>
2022-11-01 14:48:10 +0530

    Added src/ directory structure for the project.

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+/*
+ ********** Module Imports **************
+*/
+// importing core-js modules
+import "core-js/modules/es.object.values";
+// importing promise and runtime package
+import "core-js/modules/es.promise";
+import "regenerator-runtime/runtime";
+
+// importing style.scss
+import "../scss/style.scss";
+
+// alert('Hello World');
